f6daf474182ff5dd08267b2ad50e68a09629ce48
Cuongtn2 <cuongtn2@bap.jp>
2023-04-12 17:10:58 +0700

    handler error

--- complete-node-bootcamp/4-natours/starter/app.js
+++ complete-node-bootcamp/4-natours/starter/app.js
@@ -7,7 +7,8 @@ const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 app.use(cors());
 //1) MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
@@ -15,10 +16,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('heloo middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -28,6 +25,20 @@ app.use((req, res, next) => {
 // 3) ROUTER
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//khi call api mà ko nằm trong router nào khai báo
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'faild',
+  //   message: `can't find ${req.originalUrl} on this server`,
+  // });
+  // const err = new Error(`can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+//tạo middleware error handler
+app.use(globalErrorHandler);
 
 // 4) START SERVER
 module.exports = app;
---
+++ complete-node-bootcamp/4-natours/starter/controllers/errorController.js
@@ -0,0 +1,10 @@
+module.exports = (err, req, res, next) => {
+  console.log('err: ', err.message);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    message: err.message,
+    status: err.status,
+  });
+};
