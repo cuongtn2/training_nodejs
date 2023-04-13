a8defbbb1a54c29afe224194c775049478052d67
cuongtn2 <cuongtn2@bap.jp>
2023-04-13 20:16:55 +0700

    fix bug

--- complete-node-bootcamp/4-natours/starter/app.js
+++ complete-node-bootcamp/4-natours/starter/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
-const cors = require('cors');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-app.use(cors());
-//1) MIDDLEWARE
+// 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -22,23 +21,14 @@ app.use((req, res, next) => {
   next();
 });
 
-// 3) ROUTER
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-//khi call api mà ko nằm trong router nào khai báo
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'faild',
-  //   message: `can't find ${req.originalUrl} on this server`,
-  // });
-  // const err = new Error(`can't find ${req.originalUrl} on this server`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-//tạo middleware error handler
+
 app.use(globalErrorHandler);
 
-// 4) START SERVER
 module.exports = app;
