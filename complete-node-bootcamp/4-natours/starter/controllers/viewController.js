0737b0fa3224d024bcb4c2232af395e5895b69f2
hientt1 <cuongtn2@bap.jp>
2023-04-24 17:31:43 +0700

    fix

--- complete-node-bootcamp/4-natours/starter/controllers/viewController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/viewController.js
@@ -11,7 +11,15 @@ exports.getOverview = catchAsync(async (req, res) => {
 });
 
 exports.getTour = catchAsync(async (req, res) => {
+  //1) get the data for the requested tour (including review and guildes)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: ' review rating user',
+  });
+  // 2) Build template
+  // 3) render template using dât from 1
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
 });
