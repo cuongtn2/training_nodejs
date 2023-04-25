4cb86ec361e356215c35976c2fd8e582efd9e70f
cuongtn2 <cuongtn2@bap.jp>
2023-04-25 22:56:43 +0700

    delete

--- complete-node-bootcamp/4-natours/starter/controllers/viewController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/viewController.js
@@ -19,14 +19,15 @@ exports.getTour = catchAsync(async (req, res) => {
   console.log('tour', tour);
   // 2) Build template
   // 3) render template using dât from 1
-  res
-    .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
-    .render('tour', {
-      title: `${tour.name} Tour`,
-      tour,
-    });
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
+
+exports.getLoginForm = catchAsync(async (req, res) => {
+  res.status(200).render('login', {
+    title: `Log into your account`,
+    // tour,
+  });
 });
--- complete-node-bootcamp/4-natours/starter/routes/viewRoutes.js
+++ complete-node-bootcamp/4-natours/starter/routes/viewRoutes.js
@@ -6,4 +6,5 @@ router.get('/', viewsController.getOverview);
 
 router.get('/tour/:slug', viewsController.getTour);
 
+router.get('/login', viewsController.getLoginForm);
 module.exports = router;
