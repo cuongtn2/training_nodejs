706822ee667745ed55aaaef297e7eee66e71c7b7
hientt1 <cuongtn2@bap.jp>
2023-04-26 11:36:42 +0700

    fix bug

--- complete-node-bootcamp/4-natours/starter/controllers/viewController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/viewController.js
@@ -1,33 +1,67 @@
-const Tour = require('./../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+  console.log('Overview');
+  // 1) Get tour data from collection
   const tours = await Tour.find();
-  console.log('tours', tours);
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours: tours,
+    tours
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
-  //1) get the data for the requested tour (including review and guildes)
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: ' review rating user',
+    fields: 'review rating user'
   });
-  console.log('tour', tour);
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
   // 2) Build template
-  // 3) render template using dât from 1
+  // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
-    tour,
+    tour
   });
 });
 
-exports.getLoginForm = catchAsync(async (req, res) => {
+exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
-    title: `Log into your account`,
-    // tour,
+    title: 'Log into your account'
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
   });
 });
--- complete-node-bootcamp/4-natours/starter/routes/viewRoutes.js
+++ complete-node-bootcamp/4-natours/starter/routes/viewRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewController');
+const authController = require('./../controllers/authController');
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
 router.get('/', viewsController.getOverview);
 
 router.get('/tour/:slug', viewsController.getTour);
 
 router.get('/login', viewsController.getLoginForm);
+module.exports = router;
+
+// router.post(
+//   '/submit-user-data',
+//   authController.protect,
+//   viewsController.updateUserData
+// );
+
 module.exports = router;
